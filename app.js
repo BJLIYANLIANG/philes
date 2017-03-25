5df3fa763c0db857497c921e1db54530bfa256c4
Chris Matthieu <chris@matthieu.us>
2017-03-25 14:16:08 -0700

    added menu

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ process.on('uncaughtException', function(err) {
 
 var express = require('express');
 var app = express();
+var ipfs = require('ipfs-client');
 
 app.use(express.static('public'));
+
+app.get('/ipfs/*', function(req, res) {
+  // console.log(req.originalUrl.substring(6));
+  // var qm = req.originalUrl.substring(1);
+  try{
+    ipfs.cat(req.originalUrl.substring(6))
+    .pipe(res);
+  } catch(e) {
+    res.sendStatus(e);
+  }
+});
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
